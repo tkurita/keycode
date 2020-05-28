85d2c737b2c1b3cc8b25ed4ad3dbcb01acc15109
Kurita Tetsuro <tkurita@mac.com>
2020-05-28 13:21:43 +0900

    Initial Commit

---
+++ keycode/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  keycode
+//
+//  Created by 栗田 哲郎 on 2020/05/28.
+//  Copyright © 2020 tkurita. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
